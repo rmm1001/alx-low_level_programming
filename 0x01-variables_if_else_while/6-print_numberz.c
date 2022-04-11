2c07af90910361b9bd46767e00efeee8a2663093
rmm1001 <mashabela96@.com>
2022-04-11 11:10:20 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print numbers 0 to 9 on one line
+ */
+int main(void)
+{
+for ('0' + i; i <= 9; ++i)
+putchar('0' + i);
+
+putchar('\n');
+
+return (0)
+}

3d8a918a5a3f25449670f3145efea1ae480c48c9
rmm1001 <mashabela96@.com>
2022-04-11 10:53:14 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main entry point
+ * main - entry point
  *
  * Description: Print numbers 0 to 9 on one line
  * Return: 0
  */
 int main(void)
-{	
+{
 char i;
 
 for (i = '0'; i <= '9'; ++i)

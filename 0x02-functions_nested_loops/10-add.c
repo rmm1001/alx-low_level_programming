03c79cfeea4266253a1cda37c0e96afccf696a7a
rmm1001 <mashabela96@.com>
2022-04-12 14:24:46 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add . Adds two integres.
+ * @num1: the first interger to be added.
+ * @numb2: the second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int ads(int numb1, int numb2)
+{
+return (num1 + num2);
+}

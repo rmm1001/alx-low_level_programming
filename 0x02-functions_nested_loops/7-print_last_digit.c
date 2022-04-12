a14cc613fc813cd9c2fd889361bc4d85e01c9cdf
rmm1001 <mashabela96@.com>
2022-04-12 13:40:37 -0700

    There are only 3 colors, 10 digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - Prints the last digit of a number.
- * @n: The number in the question.
+ * @n: The number in question.
  *
  * Return: Value of the last digit.
  */
@@ -10,7 +10,7 @@ int print_last_digit(int n)
 {
 int last_digit = n % 10;
 
-if (lastdigit < 0)
+if (last_digit < 0)
 last_digit *= -1;
 
 _putchar(last_digit + '0');

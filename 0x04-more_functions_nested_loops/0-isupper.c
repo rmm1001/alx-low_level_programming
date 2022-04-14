27915197441e3272019da50f3e4ba76c8b3f135a
rmm1001 <mashabela96@.com>
2022-04-14 10:55:37 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * _isupper checkes upper case
+ * @c: print in
+ * Return: give 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+
+	else
+	{
+	return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+We are expected to know what nested loops works and hoe to use them.

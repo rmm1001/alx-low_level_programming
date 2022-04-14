b43cf2a307d11adfe2ba9ae9b9ff6e962897bc81
rmm1001 <mashabela96@.com>
2022-04-14 06:41:49 -0700

    Like,comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,7 +6,7 @@
  */
 
 int main(void)
-{
+{ 
 int i;
 
 printf("infinite loop incoming :(\n");
@@ -15,10 +15,11 @@ i = 0;
 
 /*
  * while (i < 10)
- * {
+ * { 
  * putchar(i);
  * }
  */
 printf("infinite loop avoided! \\o/\n");
+
 return (0);
 }

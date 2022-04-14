8f2ab6aa59820db07b1dee9a706d6df15346f0b4
rmm1001 <mashabela96@.com>
2022-04-14 12:17:19 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,13 +6,13 @@
  */
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
-	{
-	return (1);
-	}
-	else
-	{
-		return (0);
-	}
-	_putchar('\n');
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
 }

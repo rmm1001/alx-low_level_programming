b0a37ecd9d4e2e134abcec2d0c24b27c0c4a2380
rmm1001 <mashabela96@.com>
2022-04-16 03:51:56 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
-#include "main.h"
+#include "main"
 /**
- * _isdigit - checkes if the no is b/n 0 to 9
+ * _isdigit - ckeck if the no is b/n 0 to 9
  * @c: input no
- * Return: 1 if is a number (0 to 9),0 in tother case.
+ * Return: 1 is is a number (0 to 9), 0 in other case.
  */
 int _isdigit(int c)
 {
-if (c >= 48 && c <= 57)
+if (c >= 48 && c <=)
 {
 return (1);
 }
@@ -14,5 +14,5 @@ else
 {
 return (0);
 }
-_putchar('\n');
+_putchar('\n')
 }

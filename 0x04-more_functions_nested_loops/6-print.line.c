65f3775b673eb51c004d9a149070bec485f5df3e
rmm1001 <mashabela96@.com>
2022-04-18 12:29:21 -0700

    The shoartest distance between two poits is a straight line

--- 0x04-more_functions_nested_loops/6-print.line.c
+++ 0x04-more_functions_nested_loops/6-print.line.c
@@ -6,23 +6,13 @@
  */
 void print_line(int n)
 {
-
-
 int i;
 
 if (n > 0)
 {
 for (i = 0; i < n; i++)
 {
-_putchar (95);
+_putchar('-');
 }
-
-_putchar ('\n');
 }
-
-else
-{
-_putchar ('\n');
-}
-
-}
+_putchar('\n');

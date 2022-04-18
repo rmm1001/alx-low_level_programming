ef813c706d3117065caf530c57f0e53f42ac65f9
rmm1001 <mashabela96@.com>
2022-04-18 11:07:50 -0700

    i belive in numbers and signes

---
+++ 0x04-more_functions_nested_loops/4-print-most_numbers.c
@@ -0,0 +1,5 @@
+#include "main.h"
+/**
+ * print_most_nubers -print numbers
+ *
+ * Return: Always 0.
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,27 +1,23 @@
-#include "main"
-
+#include "main.h"
 /**
- * print_most_numbers - prints exept 2 and 4.
+ * print_most_numbers -print numbers
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-
 void print_most_numbers(void)
 {
+
 int i;
 
 i = 0;
-
-for (i = 0; i < 10; i++)
+while (i < 10)
 {
-
-if (i = 2 || i == 4)
-
-i++;
-
-_putchar(i + '0');
-
+if (i != 2 && i != 4)
+{
+_putchar (i + '0');
 }
-_putchar ('\n');
+i++;
+}
 
+_putchar ('\n');
 }

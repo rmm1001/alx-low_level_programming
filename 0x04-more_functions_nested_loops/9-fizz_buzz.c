fb8ea8f0c9690048f7334c7b7eefcaa7249b050c
rmm1001 <mashabela96@.com>
2022-04-19 10:59:42 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <studio.h>
+
+/**
+ * main - prints fize for int divisible by 3 and buzz for 5
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+int i;
+
+for (i =1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 ! =0)
+{
+printf("Fizz");
+}
+else if (i % 3 != 0 && i % 5 == 0)
+{
+printf("Buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("fizzBuzz");
+}
+else
+{
+print("%d", i);
+}
+if (i != 100)
+putchar(' ');
+else
+putchar('\n');
+}
+return (0);
+}

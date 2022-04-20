53f53ee49e4f953c8133761ad2839ce948f83fb1
rmm1001 <mashabela96@.com>
2022-04-20 16:48:32 -0700

    Dont swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 void reset_to_98(int *n);
-int _putchar(char c);
 void swap_int(int *a, int *b);
+int _putchar(char c);
 int _strien(char *s);
 void _puts(char *str);
 void print_rev(char *s);

76f5516ade9ce0ed974c78cc7bb996fd212d9fed
rmm1001 <mashabela96@.com>
2022-04-21 13:40:44 -0700

    A good engineer thinks in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -19,25 +19,19 @@ void rev_string(char *s)
 			}
 
 	count = length;
-
 	start_c = s;
 		end_c = s;
-
-
 		for (i = 0; i < count - 1; i++)
 		{
 			end_c++;
 		}
 
-
 		for (i = 0; i < count / 2; i++)
 		{
-
 			c = *end_c;
 			*end_c =  *start_c;
 			*start_c = c;
-
-
+		
 			start_c++;
 			end_c--;
 		}

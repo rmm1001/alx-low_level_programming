1bbbddc3555763df613efbcd3d1894fc70310863
rmm1001 <mashabela96@.com>
2022-04-21 12:26:57 -0700

    Winning is only half of it.

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - legnth of string
+ * @str:char
+ * Return: int
+ */
+
+void puts_half(char *str)
+{
+	inti, j;
+
+	i = 0;
+	while (str[i])
+		i++;
+	j = i / 2;
+	if (i % 2)
+		j += 1;
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}

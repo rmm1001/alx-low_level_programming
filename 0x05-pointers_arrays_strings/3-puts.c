33812b534f041743859a1990bfb23604b385205a
rmm1001 <mashabela96@.com>
2022-04-21 04:19:02 -0700

    I do not fear computers, I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _put -prints a string
+ * @str: Apointer to an int that will be changed 
+ *
+ * Return: void
+ */
+
+void _puts(char *str);
+{
+	char **c
+		int i;
+	for (i = 0; str[i] != '\0'; i++);
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return;
+}

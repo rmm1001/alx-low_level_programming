6a58e3ffca90891aae4c7f4161d1780834ef7f90
rmm1001 <mashabela96@.com>
2022-04-21 14:26:36 -0700

    The numbers speak for themselves

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_number(void);
+void print_line(int n);
+void print_diagonal(int n);
+
+#endif

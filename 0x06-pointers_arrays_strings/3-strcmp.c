54a87f1531c22cab5cef26a7c12e7fce30327e9f
rmm1001 <mashabela96@.com>
2022-04-22 10:20:25 -0700

    strcpm

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,14 +6,15 @@
  * @s2: string to be compared
  * Return: negative if s1 < s2; 0 if mathcing and positive int if s1 > s2
  */
+
 int _strcmp(char *s1, char *s2);
 {
-	int x;
+int x;
 
-	for (x = 0; s1[x] != '\0' || s2[x] != '\0'; x++)
-	{
-	if  (s1[x] != s2[x])
-	return (s1[x] - s2[x]);
-	}
-	return (0);
+for (x = 0; s1[x] != '\0' || s2[x] != '\0'; x++)
+{
+if  (s1[x] != s2[x])
+return (s1[x] - s2[x]);
+}
+return (0);
 }

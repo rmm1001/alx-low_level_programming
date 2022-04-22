153af077de97e7ebc678a0840a9648a82e9d27b1
rmm1001 <mashabela96@.com>
2022-04-22 06:37:56 -0700

    strtcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,13 +6,12 @@
  * @src: firts string to be concatenated to dest
  * Return: Always 0
  */
-
 char*_strcat(char *dest, char *src)
 {
 int x, y;
 
 for (x = 0; dest[x] != '\0'; x++)
-;
+
 for (y = 0; src[y] != '\0'; y++)
 dest[x] = src[y];
 x++;

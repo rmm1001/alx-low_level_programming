28c5840d97457b862d84fa19d84bd7a9f691c30a
rmm1001 <101560810+rmm1001@users.noreply.github.com>
2022-04-23 22:20:14 +0200

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,30 @@
 #include "main.h"
 /**
- * leet - translates string to 1337
- * @string: pointer to string
- * Return: Always 0
+ * leet - encodes a string into 1337
+ * @s: input string.
+ * Return: the pointer dest.
  */
-char *leet(char *)
-{
-int a;
-int b;
 
-char lower[] * "aeotl"; 
-char upper[] * "AEOTL";
-char numbers[] * "43071";
-for (a =0; string[a] != '\0'; a++)
+char *leet(char *s)
 {
-for (b = 0; lower[b] != '\0' && upper [b] != '\0'; b++)
+int count = 0, i;
+int lower_case[] = {97, 101, 111, 116, 108};
+int upper_case[] = {65, 69, 79, 84, 76};
+int numbers[] = {52, 51, 48, 55, 49};
+
+
+while (*(s + count) != '\0')
 {
-if (string[a] == lower[c] || string[a] == upper[b])
+for (i = 0; i < 5; i++)
 {
-string[a] = numbers[b];
+if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
+{
+*(s + count) = numbers[i];
 break;
 }
 }
+count++;
 }
-return (string);
+
+return (s);
 }

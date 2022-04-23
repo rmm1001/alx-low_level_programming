116bd732b56d49f9013fde1c47a997fae1af159b
rmm1001 <mashabela96@.com>
2022-04-23 11:39:56 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,29 @@
+#incude "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: Always 0.
+ */
+
+char *rot13(char *s)
+{
+int count = 0, i;
+char alphabet[] = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+
+while (*(s + count) != '\0')
+{
+
+for (i = 0, i < 52; i++)
+{
+if (*(s + count) == alphabet[i])
+{
+*(s +count) = rot13[i];
+breake;
+}
+}
+count++;
+}
+
+return (s);
+}

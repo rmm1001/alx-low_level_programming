2744555994d2d874560568e3f52aa94207aa479c
rmm1001 <101560810+rmm1001@users.noreply.github.com>
2022-04-23 23:02:46 +0200

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -8,7 +8,7 @@
  */
 int isPrintableASCII(int n)
 {
-return (n >= 32 && n <= 126);
+	return (n >= 32 && n <= 126);
 }
 
 /**
@@ -19,22 +19,22 @@ return (n >= 32 && n <= 126);
  */
 void printHexes(char *b, int start, int end)
 {
-int i = 0;
+	int i = 0;
 
-while (i < 10)
-{
-if (i < end)
-printf("%02x", *(b + start + i));
-else
-printf(" ");
-if (i % 2)
-printf(" ");
-i++;
-}
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(b + start + i));
+		else
+			printf("  ");
+		if (i % 2)
+			printf(" ");
+		i++;
+	}
 }
 
 /**
- * printASCII - print ascii values for string b
+ * printASCII - print ascii values for string b,
  * formatted to replace nonprintable chars with '.'
  * @b: string to print
  * @start: starting position
@@ -42,16 +42,16 @@ i++;
  */
 void printASCII(char *b, int start, int end)
 {
-int ch, i = 0;
+	int ch, i = 0;
 
-while (i < end)
-{
-ch = *(b + i + start);
-if (!isPrintableASCII(ch))
-ch = 46;
-printf("%c", ch);
-i++;
-}
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!isPrintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
 }
 
 /**
@@ -61,19 +61,18 @@ i++;
  */
 void print_buffer(char *b, int size)
 {
-int start, end;
+	int start, end;
 
-if (size > 0)
-{
-for (start = 0; start < size; start += 10)
-{
-end = (size - start < 10) ? size - start : 10;
-printf("%08x: ", start);
-printHexes(b, start, end);
-printASCII(b, start, end);
-printf("\n");
-}
-}
-else
-printf("\n");
+	if (size > 0)
+	{
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n");
+		}
+	} else
+		printf("\n");
 }

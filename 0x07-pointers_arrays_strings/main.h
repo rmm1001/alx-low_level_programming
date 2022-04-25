962c8f80053c186d1f46183224e4540ddf663ddc
rmm1001 <101560810+rmm1001@users.noreply.github.com>
2022-04-25 22:07:42 +0200

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,13 +3,13 @@
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
-char *memcpy(char *dest, char *src, usigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-unsigned int _strspn(char *, char *accept);
-char *strpbrk(car *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void print_daigsums(int *A, int size);
+void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 #endif

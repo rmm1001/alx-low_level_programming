a3b0b9e42a5368e1532d8beb6897d9e9cb00b454
rmm1001 <mashabela96@.com>
2022-05-03 04:30:50 -0700

    The best argument against democracy is a five-minute conversation

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - printthe name of the program
+ * @argc: count arguments
+ * @argv: arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char argv[])
+{
+
+/*Drclaring variables*/

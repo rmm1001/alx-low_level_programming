bc9ebafe7e4ce0ccebc02e47ede0a5e3ab40a1eb
rmm1001 <mashabela96@.com>
2022-05-06 04:14:47 -0700

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _calloc -allocated memory for number element size bytes
+ * @nmemb: number of element in the array
+ * @size: bytes for each position in array
+ * Return: pointer void
+ */
+void 

5d5e731e6334233356774ec3e21937284d842b64
rmm1001 <101560810+rmm1001@users.noreply.github.com>
2022-05-23 13:26:57 +0200

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,17 @@
-11111
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
+ */
+void free_listint(listint_t *head)
+{
+    listint_t *temp;
+
+    while (head)
+    {
+        temp = head->next;
+        free(head);
+        head = temp;
+    }
+}
